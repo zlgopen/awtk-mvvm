de19fefd953d0ddd6760726547650614aaac5997
xianjimli <xianjimli@gmail.com>
2019-02-23 19:03:49 +0800

    refactor

--- assets/raw/scripts/room_settings.js
+++ assets/raw/scripts/room_settings.js
@@ -4,14 +4,9 @@ function RoomSettings(req) {
   this.room = req.room || 'none';
   this.temp = req.temp || 0;
   this.humidity = req.humidity || 0;
-
-  console.log(req.room);
-  console.log(req.temp);
-  console.log(req.humidity);
 }
 
 RoomSettings.prototype["return"] = function(args) {
-  console.log("RoomSettings.prototype");
   this.req.onResult({temp:this.temp, humidity:this.humidity, room:this.room});
 
   return 0;
