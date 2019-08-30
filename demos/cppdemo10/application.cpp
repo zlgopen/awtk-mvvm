51492dd0f5a4c1cd35e893d3547fef57306f0115
xianjimli <xianjimli@gmail.com>
2019-08-30 17:32:50 +0800

    add cpp demo10

---
+++ demos/cppdemo10/application.cpp
@@ -0,0 +1,32 @@
+/**
+ * File:   application.c
+ * Author: AWTK Develop Team
+ * Brief:  application
+ *
+ * Copyright (c) 2018 - 2019  Guangzhou ZHIYUAN Electronics Co.,Ltd.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * License file for more details.
+ *
+ */
+
+/**
+ * History:
+ * ================================================================
+ * 2019-08-30 Li XianJing <xianjimli@hotmail.com> created
+ *
+ */
+
+#include "awtk.h"
+#include "mvvm/mvvm.h"
+#include "temperature.hpp"
+
+ret_t application_init() {
+  view_model_factory_register("temperature", temperature_view_model_timer_create);
+
+  return navigator_to("temperature10");
+}
+
+#include "../awtk_main.c"
---
+++ demos/cppdemo10/temperature.hpp
@@ -0,0 +1,40 @@
+﻿/**
+ * File:   temperature.h
+ * Author: AWTK Develop Team
+ * Brief:  temperature
+ *
+ * Copyright (c) 2019 - 2019  Guangzhou ZHIYUAN Electronics Co.,Ltd.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * License file for more details.
+ *
+ */
+
+/**
+ * History:
+ * ================================================================
+ * 2019-08-30 Li XianJing <xianjimli@hotmail.com> created
+ *
+ */
+
+#ifndef TK_TEMPERATURE_HPP
+#define TK_TEMPERATURE_HPP
+
+#include "mvvm/base/view_model.h"
+
+/**
+ * @method temperature_view_model_timer_create
+ * 创建temperature对象。
+ *
+ * @annotation ["constructor"]
+ * @param {navigator_request_t*} req 请求参数。
+ *
+ * @return {view_model_t} 返回view_model_t对象。
+ */
+view_model_t* temperature_view_model_timer_create(navigator_request_t* req);
+
+#define TEMPERATURE(t) ((temperature_t*)(t))
+
+#endif /*TK_TEMPERATURE_HPP*/
