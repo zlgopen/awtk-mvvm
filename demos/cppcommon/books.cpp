3fee26fd4e532c6fce8dbbcc67663d0257d2c987
lixianjing <xianjimli@hotmail.com>
2019-12-31 19:27:20 -0800

    update docs

--- demos/cppcommon/books.cpp
+++ demos/cppcommon/books.cpp
@@ -4,7 +4,7 @@
  * Author: AWTK Develop Team
  * Brief:  books view model
  *
- * Copyright (c) 2019 - 2019  Guangzhou ZHIYUAN Electronics Co.,Ltd.
+ * Copyright (c) 2019 - 2020  Guangzhou ZHIYUAN Electronics Co.,Ltd.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
