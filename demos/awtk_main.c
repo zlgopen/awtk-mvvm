0666cd19a7fba4e27477bbb0e9f2ed7167b04f42
xianjimli <xianjimli@gmail.com>
2020-02-24 15:05:17 +0800

    use awtk_main.inc

--- demos/awtk_main.c
+++ demos/awtk_main.c
@@ -22,65 +22,13 @@
 #include "awtk.h"
 #include "mvvm/mvvm.h"
 
-BEGIN_C_DECLS
-ret_t assets_init(void);
-END_C_DECLS
+#define GLOBAL_INIT() mvvm_init()
+#define GLOBAL_EXIT() mvvm_deinit()
 
-#ifdef USE_GUI_MAIN
-int gui_app_start(int lcd_w, int lcd_h) {
-  tk_init(lcd_w, lcd_h, APP_MOBILE, NULL, NULL);
-#elif defined(WIN32)
-#include <windows.h>
-int WINAPI WinMain(HINSTANCE hinstance, HINSTANCE hprevinstance, LPSTR lpcmdline, int ncmdshow) {
-  char app_root[MAX_PATH + 1];
-  path_app_root(app_root);
-  tk_init(320, 480, APP_SIMULATOR, NULL, app_root);
-#elif defined(WIN32)
-#else
-int main(void) {
-  char app_root[MAX_PATH + 1];
-  path_app_root(app_root);
-  tk_init(320, 480, APP_SIMULATOR, NULL, app_root);
-#endif
-
-//#define WITH_LCD_PORTRAIT 1
-#if defined(USE_GUI_MAIN) && defined(WITH_LCD_PORTRAIT)
-  if (lcd_w > lcd_h) {
-    tk_set_lcd_orientation(LCD_ORIENTATION_90);
-  }
-#endif /*WITH_LCD_PORTRAIT*/
-
-#ifdef WITH_LCD_LANDSCAPE
-  if (lcd_w < lcd_h) {
-    tk_set_lcd_orientation(LCD_ORIENTATION_90);
-  }
-#endif /*WITH_LCD_PORTRAIT*/
-
-#if defined(WIN32)
-#if !defined(NDEBUG)
-  {
-    AllocConsole();
-    FILE* fp = NULL;
-    freopen_s(&fp, "CONOUT$", "w+t", stdout);
-  }
-#endif /*NDEBUG*/
-#endif /*WIN32*/
-
-#ifdef WITH_FS_RES
-  system_info_set_default_font(system_info(), "default_full");
-#endif /*WITH_FS_RES*/
-
-#ifndef WITHOUT_EXT_WIDGETS
-  tk_ext_widgets_init();
-#endif /*WITHOUT_EXT_WIDGETS*/
-
-  assets_init();
-  mvvm_init();
-  application_init();
-
-  tk_run();
-
-  mvvm_deinit();
-
-  return 0;
+ret_t application_exit() {
+  log_debug("application_exit\n");
+  return RET_OK;
 }
+
+#include "awtk_main.inc"
+
