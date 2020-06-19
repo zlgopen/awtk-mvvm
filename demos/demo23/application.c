142e465776e4a7f302d49471fe8286e57bb3c38a
xianjimli <xianjimli@gmail.com>
2020-06-20 07:20:42 +0800

    update demos

--- demos/demo23/application.c
+++ demos/demo23/application.c
@@ -31,30 +31,8 @@ view_model_t* app_conf_create_view_model(navigator_request_t* req) {
   return view_model_object_wrapper_create(obj);
 }
 
-ret_t app_conf_set_defaults(void) {
-  if (app_conf_get_int("#size", 0) == 0) {
-    app_conf_set_str("network.eth0.name", "eth0");
-    app_conf_set_str("network.eth0.ip", "192.168.1.10");
-    app_conf_set_str("network.eth0.mask", "255.255.255.0");
-    app_conf_set_str("network.eth0.gateway", "192.168.0.1");
-
-    app_conf_set_str("network.eth1.name", "eth1");
-    app_conf_set_str("network.eth1.ip", "192.168.2.20");
-    app_conf_set_str("network.eth1.mask", "255.255.255.0");
-    app_conf_set_str("network.eth1.gateway", "192.168.2.1");
-
-    app_conf_set_str("network.eth2.name", "eth2");
-    app_conf_set_str("network.eth2.ip", "192.168.3.30");
-    app_conf_set_str("network.eth2.mask", "255.255.255.0");
-    app_conf_set_str("network.eth2.gateway", "192.168.3.1");
-  }
-
-  return RET_OK;
-}
-
 ret_t application_init() {
   app_conf_init_json("demo23");
-  app_conf_set_defaults();
 
   view_model_factory_register("app_conf", app_conf_create_view_model);
 
