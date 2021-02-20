bed385f55790912ac69f2618dba42f3779795ca0
lixianjing <xianjimli@hotmail.com>
2021-02-20 16:33:38 +0800

    update build scripts

--- scripts/awtk_locator.py
+++ scripts/awtk_locator.py
@@ -6,25 +6,21 @@ AWTK_ROOT = ''
 def getAwtkRoot():
     return AWTK_ROOT
 
-def getAwtkDesignerPath():
+def getAwtkSDKPath():
     env = os.environ
-    if 'AWTK_DESIGNER_PATH' in env:
-        return env['AWTK_DESIGNER_PATH']
+    if 'AWTK_SDK_PATH' in env:
+        return env['AWTK_SDK_PATH']
     else:
         return ''
 
-def getAwtkOrAwtkLinuxFbRoot(LINUX_FB):
-    if LINUX_FB:
+def getAwtkOrAwtkLinuxFbRoot(is_linux_fb):
+    if is_linux_fb:
         return locateAWTK('awtk-linux-fb')
     else:
         return locateAWTK('awtk')
 
 def locateAWTK(awtk):
     awtk_root = ''
-
-    designer_path = getAwtkDesignerPath();
-    if os.path.exists(designer_path):
-        awtk_root = designer_path + '/SDK/' + awtk
 
     if not os.path.exists(awtk_root):
         dirnames = ['../'+awtk, '../../'+awtk, '../../../'+awtk]
@@ -33,18 +29,28 @@ def locateAWTK(awtk):
                 awtk_root = dirname
                 break
 
+    if not os.path.exists(awtk_root):
+        awtk_sdk_path = getAwtkSDKPath();
+        if os.path.exists(awtk_sdk_path):
+            awtk_root = awtk_sdk_path + '/' + awtk
+
     return os.path.abspath(awtk_root)
 
 def init(ARGUMENTS = None):
     global AWTK_ROOT
-    LINUX_FB = ''
+    global LINUX_FB
 
     if ARGUMENTS:
         AWTK_ROOT = ARGUMENTS.get('AWTK_ROOT', '')
-        LINUX_FB = ARGUMENTS.get('LINUX_FB', '') != ''
-
+        LINUX_FB = ARGUMENTS.get('LINUX_FB', '')
+    else:
+        LINUX_FB = ''
+    
     if not os.path.exists(AWTK_ROOT):
-        AWTK_ROOT = getAwtkOrAwtkLinuxFbRoot(LINUX_FB)
+        AWTK_ROOT = getAwtkOrAwtkLinuxFbRoot(LINUX_FB != '')
+    elif os.path.exists(LINUX_FB):
+        print(' do not set LINUX_FB and AWTK_ROOT !!!')
+        sys.exit()
 
     if LINUX_FB:
         AWTK_SCRIPTS_ROOT = os.path.join(AWTK_ROOT, '../awtk/scripts')
