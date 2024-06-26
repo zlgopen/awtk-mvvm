534a71f3090a144f2336048853863daa21d25b10
lixianjing <xianjimli@hotmail.com>
2024-06-26 17:17:35 +0800

    improve demos

--- demos/demo14/application.c
+++ demos/demo14/application.c
@@ -23,7 +23,13 @@
 #include "mvvm/mvvm.h"
 #include "temperature_view_model.h"
 
+static ret_t mvvm_app_init(void);
+static ret_t mvvm_app_deinit(void);
+
 ret_t application_init(void) {
+#ifdef AWTK_WEB
+  mvvm_app_init();
+#endif/*AWTK_WEB*/
   view_model_factory_register("temperature_ex", temperature_view_model_create);
 
   return navigator_to("temperature14");
@@ -31,6 +37,9 @@ ret_t application_init(void) {
 
 ret_t application_exit(void) {
   log_debug("application_exit\n");
+#ifdef AWTK_WEB
+  mvvm_app_deinit();
+#endif/*AWTK_WEB*/
 
   return RET_OK;
 }
