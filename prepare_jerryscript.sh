ba71e5d85a24a2a50de1de86e2f5ed0b8c3d4d1d
lixianjing <xianjimli@hotmail.com>
2025-06-07 11:40:42 +0800

    add prepare_jerryscript.sh

---
+++ prepare_jerryscript.sh
@@ -0,0 +1,5 @@
+rm -rf 3rd/jerryscript/jerryscript
+
+git clone https://github.com/jerryscript-project/jerryscript.git 3rd/jerryscript/jerryscript
+cd 3rd/jerryscript/jerryscript
+git reset --hard 3737a28eafd580a2bee2794e4f5edd0c0471a0c6
