519df6f660617a4f9796c21e536d1473bcf830a7
xuchaoze <xuchaoze@zlg.cn>
2026-08-28 10:27:18 +0800

    improve custom binder

--- src/mvvm/base/custom_binder.c
+++ src/mvvm/base/custom_binder.c
@@ -22,9 +22,71 @@
 #include "tkc/mem.h"
 #include "tkc/utils.h"
 #include "tkc/object_default.h"
+#include "tkc/darray.h"
 #include "mvvm/base/custom_binder.h"
 
 static custom_binder_t* s_custom_binder;
+static darray_t* s_wildcard_binder;
+
+typedef struct _wildcard_binder_t {
+  char* pattern;
+  binding_context_bind_rule_t bind;
+} wildcard_binder_t;
+
+static int wildcard_binder_compare(const void* a, const void* b) {
+  wildcard_binder_t* wb = (wildcard_binder_t*)a;
+  const char* pattern = (const char*)b;
+  return_value_if_fail(wb != NULL && wb->pattern != NULL && pattern != NULL, -1);
+
+  return tk_str_cmp(wb->pattern, pattern);
+}
+
+static ret_t wildcard_binder_destroy(void* data) {
+  wildcard_binder_t* wb = (wildcard_binder_t*)data;
+  if (wb != NULL) {
+    TKMEM_FREE(wb->pattern);
+    TKMEM_FREE(wb);
+  }
+
+  return RET_OK;
+}
+
+static bool_t custom_binder_is_wildcard(const char* type) {
+  return type != NULL && (strchr(type, '*') != NULL || strchr(type, '?') != NULL);
+}
+
+static ret_t custom_binder_register_wildcard(const char* pattern,
+                                             binding_context_bind_rule_t bind) {
+  wildcard_binder_t* wb = NULL;
+  if (s_wildcard_binder == NULL) {
+    s_wildcard_binder = darray_create(5, wildcard_binder_destroy, wildcard_binder_compare);
+  }
+  return_value_if_fail(s_wildcard_binder != NULL, RET_OOM);
+
+  wb = (wildcard_binder_t*)darray_find(s_wildcard_binder, (void*)pattern);
+  if (wb != NULL) {
+    wb->bind = bind;
+    return RET_OK;
+  }
+
+  wb = TKMEM_ZALLOC(wildcard_binder_t);
+  return_value_if_fail(wb != NULL, RET_OOM);
+
+  wb->pattern = tk_str_copy(NULL, pattern);
+  if (wb->pattern == NULL) {
+    TKMEM_FREE(wb);
+    return RET_OOM;
+  }
+  wb->bind = bind;
+
+  if (darray_push(s_wildcard_binder, wb) != RET_OK) {
+    TKMEM_FREE(wb->pattern);
+    TKMEM_FREE(wb);
+    return RET_OOM;
+  }
+
+  return RET_OK;
+}
 
 ret_t custom_binder_init(void) {
   if (s_custom_binder == NULL) {
@@ -44,17 +106,42 @@ ret_t custom_binder_init(void) {
 bool_t custom_binder_exist(const char* type) {
   return_value_if_fail(s_custom_binder != NULL && type != NULL, FALSE);
 
-  return tk_object_has_prop((s_custom_binder->binders), type);
+  if (tk_object_has_prop((s_custom_binder->binders), type)) {
+    return TRUE;
+  }
+
+  if (s_wildcard_binder != NULL) {
+    uint32_t i = 0;
+    for (i = 0; i < s_wildcard_binder->size; i++) {
+      wildcard_binder_t* wb = (wildcard_binder_t*)darray_get(s_wildcard_binder, i);
+      if (wb != NULL && tk_wild_card_match(wb->pattern, type)) {
+        return TRUE;
+      }
+    }
+  }
+
+  return FALSE;
 }
 
 ret_t custom_binder_unregister(const char* type) {
   return_value_if_fail(s_custom_binder != NULL && type != NULL, RET_BAD_PARAMS);
+
+  if (custom_binder_is_wildcard(type)) {
+    if (s_wildcard_binder != NULL) {
+      return darray_remove(s_wildcard_binder, (void*)type);
+    }
+    return RET_NOT_FOUND;
+  }
 
   return tk_object_remove_prop((s_custom_binder->binders), type);
 }
 
 ret_t custom_binder_register(const char* type, binding_context_bind_rule_t bind) {
   return_value_if_fail(s_custom_binder != NULL && type != NULL && bind != NULL, RET_BAD_PARAMS);
+
+  if (custom_binder_is_wildcard(type)) {
+    return custom_binder_register_wildcard(type, bind);
+  }
 
   return tk_object_set_prop_pointer(s_custom_binder->binders, type, bind);
 }
@@ -66,9 +153,19 @@ ret_t custom_binder_bind(const char* type, binding_context_t* ctx, binding_rule_
   bind = (binding_context_bind_rule_t)tk_object_get_prop_pointer(s_custom_binder->binders, type);
   if (bind != NULL) {
     return bind(ctx, rule);
-  } else {
-    return RET_NOT_FOUND;
   }
+
+  if (s_wildcard_binder != NULL) {
+    uint32_t i = 0;
+    for (i = 0; i < s_wildcard_binder->size; i++) {
+      wildcard_binder_t* wb = (wildcard_binder_t*)darray_get(s_wildcard_binder, i);
+      if (wb != NULL && tk_wild_card_match(wb->pattern, type)) {
+        return wb->bind(ctx, rule);
+      }
+    }
+  }
+
+  return RET_NOT_FOUND;
 }
 
 ret_t custom_binder_deinit(void) {
@@ -76,8 +173,12 @@ ret_t custom_binder_deinit(void) {
 
   TK_OBJECT_UNREF(s_custom_binder->binders);
   TKMEM_FREE(s_custom_binder);
-
   s_custom_binder = NULL;
+
+  if (s_wildcard_binder != NULL) {
+    darray_destroy(s_wildcard_binder);
+    s_wildcard_binder = NULL;
+  }
 
   return RET_OK;
 }
